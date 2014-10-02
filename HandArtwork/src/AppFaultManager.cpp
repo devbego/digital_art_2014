5f66086bb7f124e14d1d4dd9fb04e7964e251362
chris <csugrue@gmail.com>
2014-10-02 18:26:20 +0200

    add app state manager

---
+++ HandArtwork/src/AppFaultManager.cpp
@@ -0,0 +1,328 @@
+//
+//  appFaultManager.cpp
+//  HandArtwork
+//
+//  Created by chris on 1/10/14.
+//
+//
+
+#include "AppFaultManager.h"
+
+
+void AppFaultManager::setup(){
+
+    timeHasFault[FAULT_NOTHING_WRONG] = 0;
+    timeHasFault[FAULT_NO_USER_PRESENT_BRIEF] = 0;
+    timeHasFault[FAULT_NO_USER_PRESENT_LONG] = 0;
+    timeHasFault[FAULT_LEAP_DROPPED_FRAME] = 0;
+    timeHasFault[FAULT_NO_LEAP_HAND_TOO_SMALL] = 0;
+    timeHasFault[FAULT_NO_LEAP_OBJECT_PRESENT] = 0;
+    timeHasFault[FAULT_TOO_MANY_HANDS] = 0;
+    timeHasFault[FAULT_HAND_TOO_FAST] = 0;
+    timeHasFault[FAULT_HAND_TOO_HIGH] = 0;
+    timeHasFault[FAULT_HAND_TOO_CURLED] = 0;
+    timeHasFault[FAULT_HAND_TOO_VERTICAL] = 0;
+    timeHasFault[FAULT_HAND_NOT_DEEP_ENOUGH] = 0;
+
+    
+    timeLimit[FAULT_NOTHING_WRONG] = 0;
+    timeLimit[FAULT_NO_USER_PRESENT_BRIEF] = 1;
+    timeLimit[FAULT_NO_USER_PRESENT_LONG] = 10;
+    timeLimit[FAULT_LEAP_DROPPED_FRAME] = 1;
+    timeLimit[FAULT_NO_LEAP_HAND_TOO_SMALL] = 1;
+    timeLimit[FAULT_NO_LEAP_OBJECT_PRESENT] = 1;
+    timeLimit[FAULT_TOO_MANY_HANDS] = 1;
+    timeLimit[FAULT_HAND_TOO_FAST] = 1;
+    timeLimit[FAULT_HAND_TOO_HIGH] = 1;
+    timeLimit[FAULT_HAND_TOO_CURLED] = 1;
+    timeLimit[FAULT_HAND_TOO_VERTICAL] = 1;
+    timeLimit[FAULT_HAND_NOT_DEEP_ENOUGH] = 1;
+    
+    bShowingFault = false;
+    
+    font.loadFont("fonts/vagrblsb.ttf", 24);
+    fontAlpha = 0;
+    myFaultString = "";
+    myFaultStringNL = "";
+    shownFault = FAULT_NOTHING_WRONG;
+    timeStartShowFault = 0.f;
+    showFaultMode = 0;
+    minTimeOn = 6;
+
+}
+
+void AppFaultManager::updateHasFault(ApplicationFault fault, float dt){
+    timeHasFault[fault] += dt;
+}
+
+void AppFaultManager::updateResetFault( ApplicationFault fault){
+    timeHasFault[fault] = 0;
+}
+
+
+ApplicationFault AppFaultManager::getLongestFault(){
+    
+    ApplicationFault activeFault = FAULT_NOTHING_WRONG;
+    float timeOn = 0;
+    
+    // find the fault on the longest and above its time limit
+    typedef std::map<ApplicationFault,float>::iterator it_type;
+    for(it_type iterator = timeHasFault.begin(); iterator != timeHasFault.end(); iterator++) {
+        ApplicationFault fault = iterator->first;
+        if( iterator->second > timeLimit[fault] && iterator->second > timeOn){
+            activeFault = fault;
+            timeOn = iterator->second;
+        }
+    }
+    
+    return activeFault;
+}
+
+// get all detected faults
+vector<ApplicationFault> AppFaultManager::getAllFaults(){
+    
+    vector<ApplicationFault> faults;
+    typedef std::map<ApplicationFault,float>::iterator it_type;
+    
+    for(it_type iterator = timeHasFault.begin(); iterator != timeHasFault.end(); iterator++) {
+        ApplicationFault fault = iterator->first;
+        if( iterator->second > timeLimit[fault]){
+            faults.push_back(fault);
+        }
+    }
+    
+    return faults;
+    
+}
+
+void AppFaultManager::drawFaultHelpScreen(){
+    
+    ApplicationFault activeFault = FAULT_NOTHING_WRONG;
+    
+    float timeOn = 0;
+    
+    // find the fault on the longest and above its time limit
+    typedef std::map<ApplicationFault,float>::iterator it_type;
+    for(it_type iterator = timeHasFault.begin(); iterator != timeHasFault.end(); iterator++) {
+        ApplicationFault fault = iterator->first;
+        if( iterator->second > timeLimit[fault] && iterator->second > timeOn){
+            activeFault = fault;
+            timeOn = iterator->second;
+        }
+    }
+    
+    
+    //cout << "activeFault " << activeFault << endl;
+    
+   if(showFaultMode == 0){
+        
+       // start new
+        switch (activeFault) {
+            case FAULT_NOTHING_WRONG:
+                myFaultString = "";
+                myFaultStringNL = "";
+                break;
+            case FAULT_NO_USER_PRESENT_BRIEF:
+                myFaultString = "Please insert your hand to begin.";
+                myFaultStringNL = " Stop je hand in het apparaat om te beginnen.";
+                break;
+            
+            case FAULT_NO_USER_PRESENT_LONG:
+                myFaultString = "Please insert your hand to begin.";
+                myFaultStringNL = "Stop je hand in het apparaat om te beginnen.";
+                break;
+            
+            case FAULT_LEAP_DROPPED_FRAME:
+                // TODO: need text for dropped frame
+                myFaultString = "";
+                myFaultStringNL = "";
+                break;
+            
+            case FAULT_NO_LEAP_OBJECT_PRESENT:
+                myFaultString = "Only hands, please.";
+                myFaultStringNL = "Alleen je hand alsjeblieft.";
+                break;
+            
+            case FAULT_TOO_MANY_HANDS:
+                myFaultString = "Just one hand at a time, please.";
+                myFaultStringNL = "E√©n hand tegelijk alsjeblieft.";
+                break;
+            
+            case FAULT_HAND_TOO_FAST:
+                myFaultString = "Try moving more slowly.";
+                myFaultStringNL = "Probeer langzamer te bewegen.";
+                break;
+            
+            case FAULT_HAND_TOO_HIGH:
+                myFaultString = "Your hand is too high up.";
+                myFaultStringNL = "Je houdt je hand te hoog.";
+                break;
+             
+            case FAULT_HAND_TOO_CURLED:
+                myFaultString = "Try keeping your hand flat.";
+                myFaultStringNL = " Probeer je hand recht te houden.";
+                break;
+                
+            case FAULT_HAND_TOO_VERTICAL:
+                myFaultString = "Try keeping your hand flat.";
+                myFaultStringNL = " Probeer je hand recht te houden.";
+                break;
+                
+            case FAULT_HAND_NOT_DEEP_ENOUGH:
+                // TODO: need text for hand not deep enough
+                myFaultString = "";
+                myFaultStringNL = "";
+                break;
+                
+            case FAULT_NO_LEAP_HAND_TOO_SMALL:
+                myFaultString = "I'm sorry! Your hand might be too small :(";
+                myFaultStringNL = "Het spijt me! Je hand is waarschijnlijk te klein voor mij :(";
+                break;
+                
+            default:
+                break;
+        }
+        
+       showFaultMode = 1;
+       shownFault = activeFault;
+       timeStartShowFault = ofGetElapsedTimef();
+   }
+    
+    
+    if(activeFault == FAULT_NOTHING_WRONG){
+        timeStartShowFault = ofGetElapsedTimef()-(minTimeOn+1);
+    }
+    
+    // update display mode
+    if( shownFault != activeFault ){
+        // we have a different fault now, turn this off if on for min time
+        if( ofGetElapsedTimef() - timeStartShowFault > minTimeOn){
+            showFaultMode = 2;
+        }
+    }else if( timeOn == 0 ){
+        if( ofGetElapsedTimef() - timeStartShowFault > minTimeOn){
+            showFaultMode = 2;
+        }
+    }
+    
+    // if alpha out, and mode is go off, set is off
+    if( showFaultMode == 2 && fontAlpha <= 0){
+        showFaultMode = 0;
+    }
+    
+    // update alpha
+    if(showFaultMode == 1 && fontAlpha < 1){
+        fontAlpha += .1;
+    }else if( showFaultMode == 2 && fontAlpha > 0){
+        fontAlpha -= .1;
+    }
+    
+    //cout << "myFaultString " << myFaultString << endl;
+    
+    // draw to screen
+    float stringWidth =  font.stringWidth(myFaultString);
+    
+    ofSetColor(255,255*powf(fontAlpha,1.5));
+    ofPushMatrix();
+        ofTranslate(50, ofGetHeight()/2+stringWidth/2.0);
+        ofRotate(-90);
+        font.drawString(myFaultString, 0,0);
+    ofPopMatrix();
+    
+    stringWidth =  font.stringWidth(myFaultStringNL);
+    
+    ofSetColor(255,255*powf(fontAlpha,.75));
+    ofPushMatrix();
+    ofTranslate(100, ofGetHeight()/2+stringWidth/2.0);
+    ofRotate(-90);
+    font.drawString(myFaultStringNL, 0,0);
+    ofPopMatrix();
+    
+    
+}
+
+
+/*
+ Please insert your hand to begin.
+ Stop je hand in het apparaat om te beginnen.
+ 
+ Put your hand in this zone.
+ Hou je hand in dit gebied.
+ 
+ Touch the screen for a new scene.
+ Raak het scherm aan voor een nieuwe sc√®ne.
+ 
+ Oops! Try moving more slowly.
+ Oeps! Probeer langzamer te bewegen.
+ 
+ Oops! Try keeping your hand flat.
+ Oeps! Probeer je hand recht te houden.
+ 
+ Oops! Your hand is too high up.
+ Oeps! Je houdt je hand te hoog.
+ 
+ Hey! Just one hand at a time, please.
+ Hee! E√©n hand tegelijk alsjeblieft.
+ 
+ Hey! Hold still for a moment, please.
+ Hee! Hou je hand stil alsjeblieft.
+ 
+ I'm sorry! Your hand might be too small :(
+ Het spijt me! Je hand is waarschijnlijk te klein voor mij :(
+ 
+ Try taking your hand out, and putting it in again.
+ Probeer eens om je hand eruit te halen en deze er weer in te stoppen.
+ 
+ Hey! Only hands, please.
+ Hee! Alleen je hand alsjeblieft.
+ 
+ Hey! That's not a hand.
+ Hee! Dat is geen hand.
+ 
+ */
+
+
+
+void AppFaultManager::drawDebug(int x, int y){
+    
+    float timeVal = timeHasFault[FAULT_NO_USER_PRESENT_BRIEF];
+    ofSetColor(0,ofMap(timeVal,0,1,0,255),255);
+    ofDrawBitmapString("No User Present Brief",x,y); y+= 15;
+    
+    timeVal = timeHasFault[FAULT_NO_USER_PRESENT_LONG];
+    ofSetColor(0,ofMap(timeVal,0,10,0,255),255);
+    ofDrawBitmapString("No User Present Long",x,y); y+= 15;
+    
+    timeVal = timeHasFault[FAULT_LEAP_DROPPED_FRAME];
+    ofSetColor(0,ofMap(timeVal,0,.1,0,255),255);
+    ofDrawBitmapString("Leap dropped frame",x,y); y+= 15;
+    
+    timeVal = timeHasFault[FAULT_NO_LEAP_HAND_TOO_SMALL];
+    ofSetColor(0,ofMap(timeVal,0,.5,0,255),255);
+    ofDrawBitmapString("Hands too small",x,y); y+= 15;
+    
+    timeVal = timeHasFault[FAULT_NO_LEAP_OBJECT_PRESENT];
+    ofSetColor(0,ofMap(timeVal,0,.5,0,255),255);
+    ofDrawBitmapString("Object present",x,y); y+= 15;
+    
+    timeVal = timeHasFault[FAULT_TOO_MANY_HANDS];
+    ofSetColor(0,ofMap(timeVal,0,.5,0,255),255);
+    ofDrawBitmapString("Too many hands",x,y); y+= 15;
+    
+    timeVal = timeHasFault[FAULT_HAND_TOO_FAST];
+    ofSetColor(0,ofMap(timeVal,0,.5,0,255),255);
+    ofDrawBitmapString("Hands too fast",x,y); y+= 15;
+    
+    timeVal = timeHasFault[FAULT_HAND_TOO_HIGH];
+    ofSetColor(0,ofMap(timeVal,0,.5,0,255),255);
+    ofDrawBitmapString("Hands too high",x,y); y+= 15;
+    
+    timeVal = timeHasFault[FAULT_HAND_TOO_CURLED];
+    ofSetColor(0,ofMap(timeVal,0,.5,0,255),255);
+    ofDrawBitmapString("Hands too curled",x,y); y+= 15;
+    
+    timeVal = timeHasFault[FAULT_HAND_TOO_VERTICAL];
+    ofSetColor(0,ofMap(timeVal,0,.5,0,255),255);
+    ofDrawBitmapString("Hands too vertical",x,y); y+= 15;
+    
+}
---
+++ HandArtwork/src/AppFaultManager.h
@@ -0,0 +1,66 @@
+//
+//  appFaultManager.h
+//  HandArtwork
+//
+//  Created by chris on 1/10/14.
+//
+//
+
+#pragma once
+#include "ofMain.h"
+
+enum ApplicationFault {
+	FAULT_NOTHING_WRONG				= 0,	/* Everything appears to be working normally */
+	FAULT_NO_USER_PRESENT_BRIEF		= 1,	/* Hand may have been momentarily withdrawn, not present for ~1 second */
+	FAULT_NO_USER_PRESENT_LONG		= 2,	/* Nobody is present in the camera nor leap, for more than ~10 seconds */
+	FAULT_LEAP_DROPPED_FRAME		= 4,	/* There was a hand in the camera (as recently as a second ago), but there's a leap dropout */
+	FAULT_NO_LEAP_HAND_TOO_SMALL	= 8,	/* There's a handlike object in the camera, but it may be too small for the leap to work */
+	FAULT_NO_LEAP_OBJECT_PRESENT	= 16,	/* Some bastard put something in the box */
+	FAULT_TOO_MANY_HANDS			= 32,	/* There's more than one hand in view */
+	FAULT_HAND_TOO_FAST				= 64,	/* The hand is moving too quickly */
+	FAULT_HAND_TOO_HIGH				= 128,	/* The hand is physically too close to the cameras */
+	FAULT_HAND_TOO_CURLED			= 256,	/* The hand is a fist or curled up, or has a curled finger */
+	FAULT_HAND_TOO_VERTICAL			= 512,	/* The hand is turned away from the camera */
+    FAULT_HAND_NOT_DEEP_ENOUGH      = 1024
+};
+
+
+class AppFaultManager{
+    
+public:
+    
+    void setup();
+    
+    // updates total time this fault continously present
+    void updateHasFault( ApplicationFault fault, float dt);
+    
+    // resets time to 0
+    void updateResetFault( ApplicationFault fault);
+
+    // draw fault screens (for some faults, give user feedback visually)
+    void drawFaultHelpScreen();
+    
+    // draws faults as bitmap text with color indicating "strength" of detection of the fault
+    void drawDebug(int x, int y);
+    
+    // get the fault that has been detected the longest amount of time
+    ApplicationFault getLongestFault();
+    
+    // get all detected faults
+    vector<ApplicationFault> getAllFaults();
+    
+    
+    map<ApplicationFault, float> timeHasFault; // total seconds fault recorded continuosly, if fault not found reset to 0
+    map<ApplicationFault, float> timeLimit; // total seconds needed to be considered active fault
+    
+    ofTrueTypeFont font;
+    float fontAlpha;
+    ApplicationFault shownFault;
+    string myFaultString, myFaultStringNL;
+    bool bShowingFault;
+    float timeStartShowFault;
+    int showFaultMode;
+    int minTimeOn;
+    
+    
+};
