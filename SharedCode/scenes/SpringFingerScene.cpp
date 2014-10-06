b73ed80215ba3bd6029629d5403402bb666e3837
chris <csugrue@gmail.com>
2014-10-06 23:01:22 +0200

    add draw preview

---
+++ SharedCode/scenes/SpringFingerScene.cpp
@@ -0,0 +1,9 @@
+//
+//  SpringFingerScene.cpp
+//  HandArtwork
+//
+//  Created by chris on 6/10/14.
+//
+//
+
+#include "SpringFingerScene.h"
---
+++ SharedCode/scenes/SpringFingerScene.h
@@ -0,0 +1,14 @@
+//
+//  SpringFingerScene.h
+//  HandArtwork
+//
+//  Created by chris on 6/10/14.
+//
+//
+
+#ifndef __HandArtwork__SpringFingerScene__
+#define __HandArtwork__SpringFingerScene__
+
+#include <iostream>
+
+#endif /* defined(__HandArtwork__SpringFingerScene__) */
