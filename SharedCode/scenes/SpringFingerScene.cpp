afd8d6aa6a389cd5f2f9f90b4eaf99fa46cdebf7
chris <csugrue@gmail.com>
2014-10-06 23:05:37 +0200

    add spring scene

--- SharedCode/scenes/SpringFingerScene.cpp
+++ SharedCode/scenes/SpringFingerScene.cpp
@@ -7,3 +7,28 @@
 //
 
 #include "SpringFingerScene.h"
+
+SpringFingerScene::SpringFingerScene(ofxPuppet* puppet, HandSkeleton* handSkeleton, HandSkeleton* immutableHandSkeleton) {
+    Scene::Scene();
+    Scene::setup("Spring Fingers", "Spring Fingers (Hand)", puppet, (Skeleton*)handSkeleton, (Skeleton*)immutableHandSkeleton);
+}
+
+
+void SpringFingerScene::setupGui() {
+	SpringFingerScene::initializeGui();
+}
+
+void SpringFingerScene::setupMouseGui() {
+	SpringFingerScene::initializeMouseGui();
+    
+}
+
+//==========================================================================
+void SpringFingerScene::update() {
+	HandSkeleton* handSkeleton = (HandSkeleton*)this->skeleton;
+	HandSkeleton* immutableHandSkeleton = (HandSkeleton*)this->immutableSkeleton;
+    
+	int mid[] = {HandSkeleton::PINKY_MID, HandSkeleton::RING_MID, HandSkeleton::MIDDLE_MID, HandSkeleton::INDEX_MID, HandSkeleton::THUMB_MID};
+	int tip[] = {HandSkeleton::PINKY_TIP, HandSkeleton::RING_TIP, HandSkeleton::MIDDLE_TIP, HandSkeleton::INDEX_TIP, HandSkeleton::THUMB_TIP};
+}
+
