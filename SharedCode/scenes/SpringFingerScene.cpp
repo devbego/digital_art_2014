7dddd795de01638fd1e57beeaeda11a1ae11a73e
chris <csugrue@gmail.com>
2014-10-07 23:38:42 +0200

    add spring scene

--- SharedCode/scenes/SpringFingerScene.cpp
+++ SharedCode/scenes/SpringFingerScene.cpp
@@ -11,6 +11,13 @@
 SpringFingerScene::SpringFingerScene(ofxPuppet* puppet, HandSkeleton* handSkeleton, HandSkeleton* immutableHandSkeleton) {
     Scene::Scene();
     Scene::setup("Spring Fingers", "Spring Fingers (Hand)", puppet, (Skeleton*)handSkeleton, (Skeleton*)immutableHandSkeleton);
+    
+    // make five springs for five fingers
+    springs.resize(9);
+    nodesMids.resize(5);
+    nodesTips.resize(5);
+    nodesExtended.resize(5);
+
 }
 
 
@@ -25,10 +32,88 @@ void SpringFingerScene::setupMouseGui() {
 
 //==========================================================================
 void SpringFingerScene::update() {
-	HandSkeleton* handSkeleton = (HandSkeleton*)this->skeleton;
+	
+    HandSkeleton* handSkeleton = (HandSkeleton*)this->skeleton;
 	HandSkeleton* immutableHandSkeleton = (HandSkeleton*)this->immutableSkeleton;
     
 	int mid[] = {HandSkeleton::PINKY_MID, HandSkeleton::RING_MID, HandSkeleton::MIDDLE_MID, HandSkeleton::INDEX_MID, HandSkeleton::THUMB_MID};
 	int tip[] = {HandSkeleton::PINKY_TIP, HandSkeleton::RING_TIP, HandSkeleton::MIDDLE_TIP, HandSkeleton::INDEX_TIP, HandSkeleton::THUMB_TIP};
+    
+    for( int i = 0; i < 5; i++){
+        
+        int indexTip = tip[i];
+        int indexMid = mid[i];
+        
+        ofPoint tipPoint = immutableSkeleton->getPositionAbsolute(indexTip);
+        ofPoint midPoint = immutableSkeleton->getPositionAbsolute(indexMid);
+        
+        nodesMids[i].setPosition(midPoint.x, midPoint.y);
+        nodesMids[i].bFixed = true;
+        
+        ofVec2f dir = tipPoint-midPoint;
+        dir.normalize();
+        ofVec2f newPos = tipPoint + 50*dir;
+        nodesExtended[i].setPosition(newPos.x, newPos.y);
+        nodesExtended[i].bFixed = true;
+
+        if(springs[i].len == 0 ){
+            nodesTips[i].setPosition(tipPoint.x, tipPoint.y);
+        }
+    }
+    
+    
+    for( int i = 0; i < 5; i++){
+        
+        int indexTip = tip[i];
+        int indexMid = mid[i];
+        ofPoint tipPoint = immutableSkeleton->getPositionAbsolute(indexTip);
+        ofPoint midPoint = immutableSkeleton->getPositionAbsolute(indexMid);
+		
+        // set up spring
+        if(springs[i].len == 0 ){
+            
+            ofVec2f lenDiff = tipPoint - midPoint;
+            if(lenDiff.length() > 1){
+                springs[i].setup(&nodesMids[i],&nodesTips[i], lenDiff.length());
+                if(i<4) springs[i+5].setup(&nodesTips[i], &nodesExtended[i], 25);
+            }
+        }
+    }
+    
+    for( int i = 0; i < 5; i++){
+        if(springs[i].len > 0 ){
+            
+            nodesTips[i].update();
+            nodesMids[i].update();
+            nodesExtended[i].update();
+            
+            springs[i].update();
+            if(i<4) springs[i+5].update();
+            
+            int indexTip = tip[i];
+            handSkeleton->setPosition(indexTip, nodesTips[i].pos, true, true);
+        }
+    }
+    
+    
+}
+
+void SpringFingerScene::draw() {
+    
+    /*
+     for( int i = 0; i < nodesTips.size(); i++){
+        nodesTips[i].draw();
+        nodesMids[i].draw();
+        nodesExtended[i].draw();
+    }
+
+    for( int i = 0; i < springs.size(); i++){
+        springs[i].draw();
+    }
+    */
+    
+}
+
+void SpringFingerScene::updateMouse(float mx, float my) {
 }
 
--- SharedCode/scenes/SpringFingerScene.h
+++ SharedCode/scenes/SpringFingerScene.h
@@ -11,34 +11,50 @@
 #include "Scene.h"
 #include "HandSkeleton.h"
 
-
+class Spring;
+class SpringNode;
 
 class SpringFingerScene : public Scene {
 protected:
 	
     
 public:
-	SpringFingerScene(ofxPuppet* puppet, HandSkeleton* handSkeleton, HandSkeleton* immutableHandSkeleton);
+	
+    SpringFingerScene(ofxPuppet* puppet, HandSkeleton* handSkeleton, HandSkeleton* immutableHandSkeleton);
 	
     void setupGui();
 	void setupMouseGui();
 	void update();
 	void updateMouse(float mx, float my);
 	void draw();
+    
+    vector<Spring> springs;
+    vector<SpringNode> nodesMids;
+    vector<SpringNode> nodesTips;
+    vector<SpringNode> nodesExtended;
 	
 };
 
-class Bob{
+class SpringNode{
+    
 public:
+    
     ofPoint pos, vel, acc;
     float damp, mass;
+    bool bFixed;
     
-    Bob(float x, float y){
-        pos.set(x,y);
+    SpringNode(){
+        pos.set(0,0);
         vel.set(0,0);
         acc.set(0,0);
-        mass = 24;
-        damp = .98;
+        mass = 12;
+        damp = .95;
+        bFixed = false;
+    }
+    
+    void setPosition(float x, float y){
+        pos.set(x,y);
+        
     }
     
     void update(){
@@ -46,134 +62,62 @@ public:
         vel *= damp;
         pos += vel;
         acc *= 0;
-        
-//        velocity.add(acceleration);
-//        velocity.mult(damping);
-//        location.add(velocity);
-//        acceleration.mult(0);
     }
     
     void applyForce( float fx, float fy){
         ofPoint force = ofPoint(fx,fy);
         force /= mass;
         acc += force;
-//        PVector f = force.get();
-//        f.div(mass);
-//        acceleration.add(f);
+    }
+    
+    void draw(){
+        ofSetColor(255,0,0);
+        ofEllipse(pos.x,pos.y,10,10);
     }
 };
 
 
- class Spring{
-
-     ofPoint anchor;
-     float len;
-     float k;
-     
-    Spring(float x, float y, float l){
-        anchor.set(x, y);
-        len = l;
+class Spring{
+    
+public:
+    ofPoint anchor;
+    float len;
+    float k;
+    
+    SpringNode * a;
+    SpringNode * b;
+    
+    Spring(){
+        len = 0;
         k = .2;
     };
     
-    void connect(Bob b) {
+    void setup(SpringNode * a, SpringNode * b, float l){
+        this->a = a;
+        this->b = b;
+        len = l;
+    }
+    
+    void update(){
         
-        ofPoint force = b.pos - anchor;
+        ofPoint force = a->pos-b->pos;
         float d = force.length();
         float stretch = d - len;
         force.normalize();
-        force *= -1 * k * stretch;
-        b.applyForce(force.x,force.y);
-//        // Vector pointing from anchor to bob location
-//        PVector force = PVector.sub(b.location, anchor);
-//        // What is distance
-//        float d = force.mag();
-//        // Stretch is difference between current distance and rest length
-//        float stretch = d - len;
-//        
-//        // Calculate force according to Hooke's Law
-//        // F = k * stretch
-//        force.normalize();
-//        force.mult(-1 * k * stretch);
-//        b.applyForce(force);
+        force.x *= -1 * k * stretch;
+        force.y *= -1 * k * stretch;
+        
+        if(!a->bFixed) a->applyForce(force.x,force.y);
+        force.x *= -1 ;
+        force.y *= -1 ;
+
+        if(!b->bFixed) b->applyForce(force.x,force.y);
     }
-
+    
+    
+    
+    void draw(){
+        ofSetColor(255,255,0);
+        ofLine(a->pos.x,a->pos.y,b->pos.x,b->pos.y);
+    }
 };
-
-
-/*
- // Nature of Code 2011
- // Daniel Shiffman
- // Chapter 3: Oscillation
- 
- // Class to describe an anchor point that can connect to "Bob" objects via a spring
- // Thank you: http://www.myphysicslab.com/spring2d.html
- 
- class Spring {
- 
- // Location
- PVector anchor;
- 
- // Rest length and spring constant
- float len;
- float k = 0.2;
- 
- // Constructor
- Spring(float x, float y, int l) {
- anchor = new PVector(x, y);
- len = l;
- }
- 
- // Calculate spring force
- void connect(Bob b) {
- // Vector pointing from anchor to bob location
- PVector force = PVector.sub(b.location, anchor);
- // What is distance
- float d = force.mag();
- // Stretch is difference between current distance and rest length
- float stretch = d - len;
- 
- // Calculate force according to Hooke's Law
- // F = k * stretch
- force.normalize();
- force.mult(-1 * k * stretch);
- b.applyForce(force);
- }
- 
- // Constrain the distance between bob and anchor between min and max
- void constrainLength(Bob b, float minlen, float maxlen) {
- PVector dir = PVector.sub(b.location, anchor);
- float d = dir.mag();
- // Is it too short?
- if (d < minlen) {
- dir.normalize();
- dir.mult(minlen);
- // Reset location and stop from moving (not realistic physics)
- b.location = PVector.add(anchor, dir);
- b.velocity.mult(0);
- // Is it too long?
- }
- else if (d > maxlen) {
- dir.normalize();
- dir.mult(maxlen);
- // Reset location and stop from moving (not realistic physics)
- b.location = PVector.add(anchor, dir);
- b.velocity.mult(0);
- }
- }
- 
- void display() {
- stroke(0);
- fill(175);
- strokeWeight(2);
- rectMode(CENTER);
- rect(anchor.x, anchor.y, 10, 10);
- }
- 
- void displayLine(Bob b) {
- strokeWeight(2);
- stroke(0);
- line(b.location.x, b.location.y, anchor.x, anchor.y);
- }
- }
-*/
